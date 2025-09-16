30f65e920d816caa7fa7e2b8e876f01fcd58a33d
LABUBU_ENJOYER <kuhtaigorochek@gmail.com>
2025-09-17 02:26:51 +0700

    Add files via upload

---
+++ 0.tasks/jaden.py
@@ -0,0 +1,8 @@
+x = input()
+if "`" in x:
+    x = x.replace("`", "uslonaogromniyhobot")
+    x1 = x.title()
+if "uslonaogromniyhobot" in x:
+    x1 = x1.replace("uslonaogromniyhobot", "`")
+    print(x1)
+
